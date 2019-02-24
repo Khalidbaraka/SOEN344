c9c207a159cc405b943755c3e704011584189e2e
AleexCh <achung95@hotmail.com>
2019-02-24 14:43:05 -0500

    #10 Added pop-up component
    Added a modal component to navbar

--- client/src/Components/AppNavbar.js
+++ client/src/Components/AppNavbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import LoginModal from './Login/LoginModal';
+import Login from './Login/Login';
 import { Link } from 'react-router-dom';
 
 class AppNavbar extends Component {
@@ -7,9 +8,18 @@ class AppNavbar extends Component {
       super(props);
       
       this.state = {
-        isOpen: false
+        isOpen: false,
+        showModal: false
       }
     }
+
+    showModal = () => {
+      this.setState({ showModal: true });
+    };
+
+    hideModal = () => {
+      this.setState({ showModal: false });
+    };
 
     toggle = (e) => {
       e.preventDefault();
@@ -43,12 +53,15 @@ class AppNavbar extends Component {
                     </li>
                   </ul>
                    <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                    <li className="nav-item" >
-                      <Link to={'/login'} className="nav-link"> Login </Link>
-                    </li>
+                    <li className="nav-item" onClick={this.showModal}> Login </li>
                     </ul>
                 </div>
               </nav>
+              <LoginModal isOpen={this.state.showModal} onClose={() => this.hideModal()}>
+              <button type="button" class="close" data-dismiss="modal" aria-hidden="true" onClick={() => this.hideModal()}>×</button>
+                <Login/>
+                
+              </LoginModal>
             </div>
         );
     }
