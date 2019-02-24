ade2d6cddc28d0d6fd3ba95804e9f6e7f5070e87
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2019-02-24 17:47:39 -0500

    links to handle the two sign up routes, Patient and Nurse

--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
-
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class SignUp extends Component{
        render() {
        
         return (
-            <div className="container-fluid">
+            <div>
 
-            Sign Up
+            <Row className="text-center align-middle" style={{ lineHeight: '250px' }}>
+                    <div>
+                        .
+                    </div>
+            </Row>
+            <Row className="text-center align-middle" >
+                <Col></Col>
+                <Col>
+                        
+                        <Button variant="primary m1" size="lg">
+                        Patient Sign Up
+                        </Button>
+                        
+                       
+                </Col>
+                <Col>
+                    <Button variant="secondary m1" size="lg">
+                            Nurse Sign Up
+                    </Button>
+                
+                </Col>
+                <Col></Col>
+            </Row>
+
+
             </div>
+
+            
+
+           
         );
       }
 }
