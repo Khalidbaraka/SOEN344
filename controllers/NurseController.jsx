d7bfeb5d1d3baa1a4081e6fafc7f8249e1d115d1
Shawn Guerra-Bautista <ShawnGuerraBautista@gmail.com>
2019-02-25 18:59:24 -0500

    [#20] Added encryption for password

--- controllers/NurseController.jsx
+++ controllers/NurseController.jsx
@@ -1,5 +1,6 @@
 // Nurse Model
 const Nurse = require('./../models/Nurse');
+let bcryptjs = require('bcryptjs');
 
 // Display list of all Nurse
 exports.nurse_list = (req, res) => {
@@ -23,8 +24,15 @@ exports.nurse_register = (req, res) => {
                     accessID: req.body.accessID,
                     password: req.body.password
                 });
-                newNurse.save()
-                    .then(nurse => res.json(nurse));
+                bcryptjs.genSalt(10, (err, salt)) =>{
+                    bcryptjs.hash(newNurse.password, salt, (err, hash) => {
+                        if(err){
+                            throw err;
+                        }
+                        newNurse.password=hash;
+                        newNurse.save().then(nurse => res.json(nurse));
+                    })
+                }
             }else{
                 return res.status(400).json({ accessID: 'accessID is already in the system' });
             }
@@ -34,7 +42,18 @@ exports.nurse_register = (req, res) => {
 // Edit Password
 exports.change_nurse_password = (req, res) => {
     Nurse.findOneAndUpdate({ accessID: req.body.accessID }, { $set: { password: req.body.password } })
-        .then(nurse => res.json(nurse))
+        .then(nurse => {
+            bcryptjs.genSalt(10, (err, salt)) =>
+            {
+                bcryptjs.hash(nurse.password, salt, (err, hash) => {
+                    if (err) {
+                        throw err;
+                    }
+                    nurse.password = hash;
+                    res.json(nurse)
+                })
+            }
+        })
 };
 
 // Delete Nurse
