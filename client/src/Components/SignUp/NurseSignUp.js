87cf4d849a096d4bbe1154df147161c7f1ae6e62
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2019-02-24 19:04:20 -0500

    onClick Nurse Sign Up fading text

---
+++ client/src/Components/SignUp/NurseSignUp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class NurseSignUp extends Component{
+       render() {
+       
+        return (
+            <div>
+
+            Nurse Sign Up
+
+
+            </div>
+
+            
+
+           
+        );
+      }
+}
+export default NurseSignUp;
--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Fade from 'react-bootstrap/Fade'
+import Form from 'react-bootstrap/Form'
+import NurseSignUp from './NurseSignUp';
 
 class SignUp extends Component{
+    constructor(props, context) {
+        super(props, context);
+    
+        this.state = {
+          open: false,
+        };
+      }
+
        render() {
-       
+       const { open } = this.state;
         return (
             <div>
 
@@ -25,12 +36,50 @@ class SignUp extends Component{
                        
                 </Col>
                 <Col>
-                    <Button variant="secondary m1" size="lg">
-                            Nurse Sign Up
-                    </Button>
+
+                
+                     
+                <Button
+                    onClick={() => this.setState({ open: !open })}
+                    aria-controls="example-fade-text"
+                    aria-expanded={open}
+                    >
+                    Nurse Sign Up
+                </Button>
+                <Fade in={this.state.open}>
+                    <div id="example-fade-text">
+                        
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Access ID</Form.Label>
+                                <Form.Control type="email" placeholder="Enter ID" />
+                                <Form.Text className="text-muted">
+                                ex: DOL96315
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+
+                    </div>
+                </Fade>
+
+
                 
                 </Col>
-                <Col></Col>
+                <Col>
+               
+                
+                
+                
+                </Col>
             </Row>
 
 
