2e64d48fb962fe3ef4b2c1e063bcd400121fd4d8
Jason Chung <jasonckc07@gmail.com>
2019-02-24 19:43:14 -0500

    #11 Refactor code and fixed import

--- client/src/Components/SignUp/NurseSignUp.js
+++ client/src/Components/SignUp/NurseSignUp.js
@@ -1,21 +1,35 @@
+import { Button, Form } from 'react-bootstrap';
 import React, { Component } from 'react';
 
-
 class NurseSignUp extends Component{
-       render() {
-       
-        return (
-            <div>
+  constructor(props) {
+    super(props);
+    
+  }
+  
+  render() {
+    return (
+      <div>
+          <Form>
+              <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Access ID</Form.Label>
+                  <Form.Control type="email" placeholder="Enter ID" />
+                  <Form.Text className="text-muted">
+                    ex: DOL96315
+                  </Form.Text>
+              </Form.Group>
 
-            Nurse Sign Up
-
-
-            </div>
-
-            
-
-           
-        );
-      }
+              <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              
+              <Button variant="primary" type="submit">
+                  Submit
+              </Button>
+          </Form>
+      </div>
+    );
+  }
 }
 export default NurseSignUp;
--- client/src/Components/SignUp/SignUp.js
+++ client/src/Components/SignUp/SignUp.js
@@ -1,93 +1,46 @@
+import {Button, Col, Container, Fade, Row} from 'react-bootstrap'
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Fade from 'react-bootstrap/Fade'
-import Form from 'react-bootstrap/Form'
+
 import NurseSignUp from './NurseSignUp';
 
 class SignUp extends Component{
-    constructor(props, context) {
-        super(props, context);
+    constructor(props) {
+        super(props);
     
         this.state = {
           open: false,
         };
-      }
+    }
 
-       render() {
-       const { open } = this.state;
+    render() {
+        const { open } = this.state;
+
         return (
             <div>
-
-            <Row className="text-center align-middle" style={{ lineHeight: '250px' }}>
-                    <div>
-                        .
-                    </div>
-            </Row>
-            <Row className="text-center align-middle" >
-                <Col></Col>
-                <Col>
-                        
-                        <Button variant="primary m1" size="lg">
-                        Patient Sign Up
-                        </Button>
-                        
-                       
-                </Col>
-                <Col>
-
-                
-                     
-                <Button
-                    onClick={() => this.setState({ open: !open })}
-                    aria-controls="example-fade-text"
-                    aria-expanded={open}
-                    >
-                    Nurse Sign Up
-                </Button>
-                <Fade in={this.state.open}>
-                    <div id="example-fade-text">
-                        
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label>Access ID</Form.Label>
-                                <Form.Control type="email" placeholder="Enter ID" />
-                                <Form.Text className="text-muted">
-                                ex: DOL96315
-                                </Form.Text>
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                            
-                            <Button variant="primary" type="submit">
-                                Submit
+                <Container>
+                    <Row className="text-center align-middle my-5" >
+                        <Col md={{ span: 4, offset: 2 }}>
+                            <Button variant="primary m1">
+                                Patient Sign Up
+                            </Button>     
+                        </Col>
+                        <Col md={4}>                     
+                            <Button
+                                onClick={() => this.setState({ open: !open })}
+                                aria-controls="example-fade-text"
+                                aria-expanded={open}
+                                >
+                                Nurse Sign Up
                             </Button>
-                        </Form>
-
-                    </div>
-                </Fade>
-
-
-                
-                </Col>
-                <Col>
-               
-                
-                
-                
-                </Col>
-            </Row>
-
-
-            </div>
-
-            
-
-           
+                            <Fade in={this.state.open} className="my-5">
+                                <div id="example-fade-text">
+                                    <NurseSignUp/>
+                                </div>
+                            </Fade>                
+                        </Col>
+                    </Row>
+                </Container>
+            </div>           
         );
       }
 }
