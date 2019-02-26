b1f2b7281be521a536338df8158f70be0758ddfa
Nirmal <nirmal_864@hotmail.com>
2019-02-26 01:33:11 -0500

    [#23] fetch data from API using axios in register and login

--- client/src/Components/Login/NurseLogin.js
+++ client/src/Components/Login/NurseLogin.js
@@ -2,15 +2,68 @@ import React, { Component } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
+//nurse login function
+export const login = nurse => {
+    return axios
+    .post('api/nurses/login', {
+        first_name: nurse.first_name,
+        password: nurse.password
+    })
+    .then(res => {
+        console.log("logged in")
+
+    })
+    .catch(err => {
+        console.log(err)
+    })
+}
+
 class NurseLogin extends Component{
-	render(){
+
+    constructor(){
+        super()
+        this.state = {
+            accessID: '',
+            password: '', 
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e)
+    {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    //go to home page after submitting 
+    onSubmit(e)
+    {
+        e.preventDefault()
+
+        const nurse = {
+            accessID: this.state.accessID,
+            password: this.state.password
+        }
+
+        login(nurse).then(res =>
+        {
+            if(res){
+                // go to home page 
+                // this.props.history.push('/NurseHomePage')
+            }
+        })
+    }
+    
+
+    render(){
 		console.log("Logging Nurse");
 		return(
             <div className="container">
-               <Form>
+               <Form noValidate onSubmit = {this.onSubmit}>
+
               <Form.Group controlId="formBasicUsername">
                   <Form.Label>Access ID</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Access ID" />
+                  <Form.Control name="accessID" type="text" placeholder="Enter Access ID" value = {this.state.accessID} onChange={this.onChange}/>
                   <Form.Text className="text-muted">
                      ex: DOL96315
                   </Form.Text>
@@ -18,7 +71,7 @@ class NurseLogin extends Component{
 
               <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Enter Password" />
+                  <Form.Control name="password" type="password" placeholder="Enter Password" value = {this.state.accessID} onChange={this.onChange} />
               </Form.Group>
               
               <Button variant="primary" type="submit">
--- client/src/Components/SignUp/NurseSignUp.js
+++ client/src/Components/SignUp/NurseSignUp.js
@@ -1,29 +1,78 @@
 import { Button, Form } from 'react-bootstrap';
 import React, { Component } from 'react';
+import axios from 'axios';
+
+//nurse register function
+export const register = newNurse => {
+  return axios
+  .post('api/nurses/register', {
+      first_name: newNurse.first_name,
+      last_name: newNurse.last_name,
+      accessID: newNurse.accessID,
+      password: newNurse.password
+  })
+  .then(res => {
+      console.log("successfully registered")
+  })
+}
 
 class NurseSignUp extends Component{
-  constructor(props) {
-    super(props);
+  constructor() {
+    super()
+    this.state = {
+        first_name: '',
+        last_name: '',
+        accessID: '',
+        password: '', 
+    }
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
     
   }
   
+  onChange(e)
+  {
+      this.setState({[e.target.name]: e.target.value})
+  }
+
+  //go to home page after submitting 
+  onSubmit(e)
+  {
+      e.preventDefault()
+
+      const nurse = {
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          accessID: this.state.accessID,
+          password: this.state.password
+      }
+
+      register(nurse).then(res =>
+      {
+          if(res){
+              // go to nurse home page after completing registration 
+              // this.props.history.push('/NurseHomePage')
+          }
+      })
+  }
+
   render() {
     return (
       <div>
-          <Form>
+          <Form noValidate onSubmit = {this.onSubmit}>
               <Form.Group controlId="formFirstName">
                   <Form.Label>First name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter First Name" />
+                  <Form.Control name = "first_name" type="text" placeholder="Enter First Name" value = {this.state.first_name} onChange={this.onChange} />
               </Form.Group>
 
               <Form.Group controlId="formLastName">
                   <Form.Label>Last name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Last Name" />
+                  <Form.Control name = "last_name" type="text" placeholder="Enter Last Name" value = {this.state.last_name} onChange={this.onChange} />
               </Form.Group>
 
               <Form.Group controlId="formBasicUsername">
                   <Form.Label>Access ID</Form.Label>
-                  <Form.Control type="text" placeholder="Enter ID (3 letters followed by 5 digits)" />
+                  <Form.Control name = "accessID" type="text" placeholder="Enter ID (3 letters followed by 5 digits)" value = {this.state.accessID} onChange={this.onChange} />
                   <Form.Text className="text-muted">
                      ex: DOL96315
                   </Form.Text>
@@ -31,7 +80,7 @@ class NurseSignUp extends Component{
 
               <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Form.Control name = "password" type="password" placeholder="Password" value = {this.state.password} onChange={this.onChange} />
               </Form.Group>
               
               <Button variant="primary" type="submit">
