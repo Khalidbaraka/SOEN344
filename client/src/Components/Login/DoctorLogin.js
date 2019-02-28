52a57364c90c1f4c73ed39568df0c4801cfdeff6
TheAlexRobert <alexandrerobertsmeets@gmail.com>
2019-02-28 14:17:39 -0500

    constructor and axios for Dr

--- client/src/Components/Login/DoctorLogin.js
+++ client/src/Components/Login/DoctorLogin.js
@@ -3,6 +3,60 @@ import { Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 class DoctorLogin extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            accessID: '',
+            password: '', 
+            message: '', 
+            isAuthenticated: false
+        }
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onChange(e)
+    {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    //go to home page after submitting 
+    onSubmit(e)
+    {
+        e.preventDefault()
+
+        const nurse = {
+            accessID: this.state.accessID,
+            password: this.state.password
+        }
+        axios
+         .post('api/nurses/login', {
+        accessID: nurse.accessID,
+        password: nurse.password
+         })
+        .then(res => {
+            if (res.data.success) {
+                this.setState({
+                    isAuthenticated: true
+                });
+            }
+            else{
+                this.setState({
+                    accessID: '',
+                    password: '',
+                    message: res.data.message
+                });
+            }
+
+        })
+         .catch(err => {
+        console.log(err.res)
+        })
+    
+    }
+
+    
 	render(){
 		console.log("Logging Doctor");
 		return(
