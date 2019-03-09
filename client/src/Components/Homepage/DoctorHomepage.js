252072b103be82f025c92961a7e0c7d978c81e45
Refat Kamal <refat.k17@gmail.com>
2019-03-09 18:04:05 -0500

    #53 add Form Input for date

--- client/src/Components/Homepage/DoctorHomepage.js
+++ client/src/Components/Homepage/DoctorHomepage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
-import {Link} from "react-router-dom";
 import UpdateSchedule from "../Schedule/UpdateSchedule";
+
+import moment from 'moment';
 
 class DoctorHomepage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            timeslots: null,
+            timeslots: [],
+            selectedTimeslot: null,
             showScheduleEditModal: false,
         };
 
@@ -17,20 +19,39 @@ class DoctorHomepage extends Component {
     }
 
     componentDidMount() {
-    //    TODO get doctors timeslots and set on timeslots state
+    //    TODO get doctors timeslots and set on timeslots state (for now I push 1 temp value)
+
+        let temp = {
+            doctor: null,
+            start: moment().toDate(),
+            end: moment().add(2, "hours").toDate(),
+        };
+
+        this.setState({ timeslots: [...this.state.timeslots, temp] });
     }
 
     openEdit() {
-        this.setState({showScheduleEditModal: true});
+        // TODO for now always a fixed slot, but the selected ts should be passed here
+        let arrayIndexGottenFromfunc = 0;
+        console.log(this.state.timeslots[arrayIndexGottenFromfunc]);
+        this.setState({
+            selectedTimeslot: this.state.timeslots[arrayIndexGottenFromfunc],
+            showScheduleEditModal: true,
+        });
     }
 
     closeEdit() {
-        this.setState({showScheduleEditModal: false});
+        this.setState({
+            selectedTimeslot: null,
+            showScheduleEditModal: false,
+        });
     }
 
     render() {
         return (
             <div>
+                {/*for test*/}
+                <div><pre>{JSON.stringify(this.state, null, 2) }</pre></div>
                 <h1> Doctor Homepage </h1>
 
                 {/* Many of these would exist, per timeslot and load the modal with the timeslot info */}
@@ -42,6 +63,7 @@ class DoctorHomepage extends Component {
                 <UpdateSchedule
                     show={this.state.showScheduleEditModal}
                     onHide={this.closeEdit}
+                    timeslot={this.state.selectedTimeslot}
                 />
             </div>
         );
--- client/src/Components/Schedule/UpdateSchedule.js
+++ client/src/Components/Schedule/UpdateSchedule.js
@@ -3,31 +3,65 @@ import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
 import Button from "react-bootstrap/Button";
 
+import moment from 'moment';
 
 class UpdateSchedule extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            timeslot: null,
+            date: "",
         };
+
+        this.dateChange = this.dateChange.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+    }
+
+    onOpen() {
+        this.setState({date: moment(this.props.timeslot.start).format(moment.HTML5_FMT.DATE)});
+    }
+
+    getDate() {
+        return moment(this.props.timeslot.start).format(moment.HTML5_FMT.DATE);
+    }
+
+    dateChange(event) {
+        this.setState({date: event.target.value});
     }
 
     render() {
         return (
             <div>
-                <Modal show={this.props.show} onHide={this.props.onHide}>
+                {/*for test*/}
+                <div><pre>{JSON.stringify(this.props, null, 2) }</pre></div>
+                <Modal
+                    show={this.props.show}
+                    onHide={this.props.onHide}
+                    onEnter={this.onOpen}
+                    centered
+                >
+                    <Form>
                     <Modal.Header closeButton>
                         <Modal.Title>Edit Schedule Timeslot</Modal.Title>
                     </Modal.Header>
 
                     <Modal.Body>
-                        Body
+                        <Form.Group>
+                            <Form.Label>Date</Form.Label>
+                            <Form.Control
+                                required
+                                type="date"
+                                placeholder="Date"
+                                value={this.state.date}
+                                onChange={this.dateChange}
+                            />
+                        </Form.Group>
                     </Modal.Body>
 
                     <Modal.Footer>
                         Footer
                     </Modal.Footer>
+                    </Form>
                 </Modal>
             </div>
         )
