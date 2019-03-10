e316b0e2d359a3e8376bd43bb7636a6a127c0823
AleexCh <achung95@hotmail.com>
2019-03-09 20:12:15 -0500

    #80 Updated helper function

--- controllers/HelperController.js
+++ controllers/HelperController.js
@@ -1,13 +1,10 @@
 module.exports = {
     //Method verifies if parameters (start, end) representing date for possible new timeslot overlaps with existing timeslot
     overlaps: function(appStart, appEnd, start, end) {
-        
-        if ((appStart >= start && appStart < end) 
+        console.log("insdie helper function")
+        return ((appStart >= start && appStart < end) 
             || (appEnd > start && appEnd <= end) 
             || (start >= appStart && start < appEnd) 
-            || (end > appStart && end <= appEnd)) {
-
-            return true;
-        }
+            || (end > appStart && end <= appEnd));        
     },
 };
