72507a6c87863f3a299905826e8d65ad4f5f83c9
khalid baraka <baraka.khalid4@gmail.com>
2019-03-11 16:09:18 -0400

    #49 Cart and CartList created
    #49 Cart and CartList created

--- client/src/Components/Cart/Cart.js
+++ client/src/Components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Card, Col, Dropdown, DropdownButton, Form, Row } from 'react-bootstrap';
+import Table from "react-bootstrap/Table";
+import AppointmentList from "../Appointment/AppointmentList";
 
 class Cart extends Component {
 
@@ -7,32 +9,67 @@ class Cart extends Component {
         super(props);
 
         this.state = {
-            message: "Your Cart"
+            message: "Your Cart",
+            appointments:[
+                {type: "Annual",
+                    clinic: null,
+                    doctor: "James",
+                    room: "2",
+                    start : "2019-03-20T21:33:00.000Z",
+                    patient : "Kelly",
+                    end : "2019-03-20T22:00:00.000Z",
+                    duration : "40",
+                    price : 60},
+                {type: "Walk-In",
+                    clinic: null,
+                    doctor: "Jenny",
+                    room: "2",
+                    start : "2019-10-20T21:40:00.000Z",
+                    patient : "Kelly",
+                    end : "2019-10-20T22:00:00.000Z",
+                    duration : "40",
+                    price : 60},
+                {type: "Annual",
+                    clinic: null,
+                    doctor: "James",
+                    room: "2",
+                    start : "2019-03-20T21:33:00.000Z",
+                    patient : "Kelly",
+                    end : "2019-03-20T22:00:00.000Z",
+                    duration : "40",
+                    price : 60}
+            ]
+
+
+
         }
+
     }
+
     render() {
-        const { message} = this.state;
+        const { appointments, message} = this.state;
         return (
-            
-            <div className="container">
-                { message ?
-                    <Card border="danger" className="text-center my-3">
-                        <Card.Body>
-                            <Card.Title><div className="text-monospace">{ message }</div> </Card.Title>
-                        </Card.Body>
-                    </Card>
-                    : ''}
-            <Card className="my-5">
+                    <div className="container">
+                        { message ?
+                            <Card border="danger" className="text-center my-3">
+                                <Card.Body>
+                                    <Card.Title><div className="text-monospace">{ message }</div> </Card.Title>
+                                </Card.Body>
+                            </Card>
+                            : ''}
+                        <Card className="my-5">
+                            <Card.Header>
+                                <Card.Title className="text-center text-monospace"> Appointments</Card.Title>
+                            </Card.Header>
 
-                <Card.Header>
-                        <Card.Title className="text-center text-monospace">Appointments you have selected</Card.Title>
-                </Card.Header>
+                            <AppointmentList appointments={appointments} />
+                        </Card>
+                    </div>
 
-                {/* <CartList appointments={appointments} /> */}
-            </Card> 
-            </div>
+
         );
     }
 }
 
 export default Cart;
+
